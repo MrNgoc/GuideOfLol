eb2da8eb9ffe278a04abe403ff9c8d6ed790d9bf
MrNgoc <admin@Admins-MacBook-Pro.local>
2016-08-03 10:07:08 +0700

    lol

---
+++ GuideOfLol/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  GuideOfLol
+//
+//  Created by Admin on 8/3/16.
+//  Copyright © 2016 MrNgoc. All rights reserved.
+//
+
+import UIKit
+
+
--- GuideOfLol/ViewController.swift
+++ GuideOfLol/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getData() {
+        
+        let urlRequest = NSMutableURLRequest(URL: NSURL(string: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=RGAPI-905251DD-5545-48D0-9598-0E601CA5E9AF")!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        session.dataTaskWithRequest(urlRequest) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }else{
+                if let responseHTTP = response as? NSHTTPURLResponse{
+                    if responseHTTP.statusCode == 200{
+                        guard let imageInformation = data else {return}
+                        
+                        do{
+                            let result = try NSJSONSerialization.JSONObjectWithData(imageInformation, options: NSJSONReadingOptions.AllowFragments)
+                            
+                            print(result)
+                        }catch {
+                        
+                        }
+                    }
+                }
+            }
+            
+        }.resume()
+        
+    }
+    
 }
-
